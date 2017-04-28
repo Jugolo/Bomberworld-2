4905b6bcff4169db5f8ba7cc7ef13119ca36ce7b
Bomberworld <michael.wittouck@hotmail.com>
2017-04-28 18:45:07 +0200

    Button.js

--- bombermania-client/src/modules/ui/UIButton.js
+++ bombermania-client/src/modules/ui/UIButton.js
@@ -1,13 +1,28 @@
-function UIButton( game, width, height, color, label ){
+function UIButton( game, width, height, color, label, image = ""){
 	Phaser.Group.call(this, game);
+    
+    if (image == "") {
+	    this.bg = new ColorRect(game, width, height, color, Math.min(width, height) * 0.5);
+    } else {
+        this.bg = game.add.sprite(0, 0, image);
+        this.bg.width = width;
+        this.bg.height = height;
+    }
 
-	this.bg = new ColorRect(game, width, height, color, Math.min(width, height) * 0.5);
-	this.bg.inputEnabled = true;
-	this.bg.input.useHandCursor = true;
+    this.bg.inputEnabled = true;
+    this.bg.input.useHandCursor = true;
 	
 	this.bg.events.onInputDown.add( function(){
 		this.onPress();
 	}, this );
+    
+    this.bg.events.onInputOver.add( function(){
+        image != "" && this.bg.loadTexture(image + "_over", 0);
+    }, this );
+
+    this.bg.events.onInputOut.add( function(){
+        image != "" && this.bg.loadTexture(image, 0);
+    }, this );
 
 	var font_style = { font: "28px Luckiest", fill: "#FFFFFF" };
 
