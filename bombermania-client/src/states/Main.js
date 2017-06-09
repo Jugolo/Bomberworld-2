5fb864f26849bcf9779190c107b031c57b42715a
Bomberworld <michael.wittouck@hotmail.com>
2017-06-09 02:58:28 +0200

    Update Main.js

--- bombermania-client/src/states/Main.js
+++ bombermania-client/src/states/Main.js
@@ -90,7 +90,7 @@ Retoosh.Main.prototype = {
         play_btn.y = ( Retoosh.HEIGHT - play_btn.height ) * 0.5 + 130;
 
         play_btn.onPress = function(){
-          USERNAME = "Guest"+ Math.floor(Math.random() * 90540) + 1 ;
+          USERNAME = "Guest"+ Math.floor(Math.random() * 90540) + 1 ; //nickname == "" ? upper_menu.getUsername() : nickname;
           var nickname = upper_menu.getUsername();
           USERNAME = nickname == "" ? "Guest" : nickname;
 
